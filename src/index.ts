9d776f36df9391970ab0f5819c40d70c3116f85a
cdydayang <cdy_mytest@163.com>
2021-01-10 16:47:06 +0800

    优化 :lipstick:

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,75 @@
 
-type Gneder = "male" | "female";
-
-type User = {
-  name: string,
-  age: number,
-  gender: Gneder
+enum Color {
+  plum = "♣",
+  spade = "♠",
+  block = "♦",
+  heart = "♥"
 }
 
-function getUsers(g: Gneder): User[] {
-  return [];
+type NormalCard = {
+  color: Color,
+  mark: number
 }
+
+type Deck = NormalCard[]
+
+function createDeck(): Deck {
+
+  let deck: Deck = []
+
+
+  for (let index = 1; index < 13; index++) {
+
+    deck.push({
+      mark: index,
+      color: Color.block
+    })
+    deck.push({
+      mark: index,
+      color: Color.heart
+    })
+    deck.push({
+      mark: index,
+      color: Color.plum
+    })
+    deck.push({
+      mark: index,
+      color: Color.spade
+    })
+
+  }
+
+  return deck
+
+}
+
+function printDeck(deck: Deck) {
+
+  let str: string = ""
+  deck.forEach((item, index) => {
+    if (item.mark > 1 && item.mark <= 10) {
+      str += item.color + item.mark + "\t"
+    } else if (item.mark === 11) {
+      str += item.color + "J" + "\t"
+    } else if (item.mark === 12) {
+      str += item.color + "Q" + "\t"
+    } else if (item.mark === 13) {
+      str += item.color + "K" + "\t"
+    } else {
+      str += item.color + "A" + "\t"
+    }
+
+    if ((index + 1) % 4 === 0) {
+      str += "\n\r"
+    }
+  })
+
+  console.log(str);
+
+}
+
+
+let decks = createDeck();
+
+printDeck(decks);
+
